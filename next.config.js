69e8e26069cd9d500952391f70fe48f4b79f711d
Aaron Boodman <aaron@aaronboodman.com>
2022-04-07 16:20:04 -1000

    wip

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // Prefer loading of ES Modules over CommonJS
+  experimental: { esmExternals: true },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
