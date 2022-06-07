1b0b42699a631942990b17a6edf650d5b9a3e95f
Aaron Boodman <aaron@aaronboodman.com>
2022-06-07 02:34:17 -1000

    Store selected room in cookie so user will tend to revist.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,22 @@
 import { nanoid } from "nanoid";
+import { GetServerSideProps } from "next";
 
 function Page() {
   return "";
 }
 
-export function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  let r = context.req.cookies["room"];
+  if (!r) {
+    r = nanoid(6);
+    context.res.setHeader("set-cookie", `room=${r}`);
+  }
   return {
     redirect: {
-      destination: `/d/${nanoid(6)}`,
+      destination: `/d/${r}`,
       permanent: false,
     },
   };
-}
+};
 
 export default Page;
