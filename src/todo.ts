5dc549b68c379510f6dc10d1151e7fd82e07d008
Aaron Boodman <aaron@aaronboodman.com>
2022-07-28 16:30:23 -1000

    Move custom mutator definition into mutators.ts and rewrite comments.
    I think it is more understandable if all the mutator comments and
    custom mutator are in the mutators file and todo.ts is just about
    entity definition.

--- src/todo.ts
+++ src/todo.ts
@@ -1,58 +1,35 @@
-// This file defines our "Todo" entity and its operations.
-//
-// Much of this is generated using @rocicorp/rails, a helper library which can
-// generate a CRUD API for Replicache given just a type definition. See
-// https://github.com/rocicorp/rails for more information.
+// This file defines our "Todo" entity.
+// You will generally have one such file for each entity in your application.
 
 import { z } from "zod";
 import { entitySchema, generate, Update } from "@rocicorp/rails";
-import { WriteTransaction } from "replicache";
 
-// Define the todo datatype using the zod schema language.
-// See https://zod.dev/ for documentation.
+// We define our datatypes using https://zod.dev/. This isn't required to use
+// Replicache, but is recommended. It gets you free parsing/validation of JSON
+// into strong TypeScript types and is also used below to generate helper CRUD
+// functions.
 export const todoSchema = entitySchema.extend({
   text: z.string(),
   completed: z.boolean(),
   sort: z.number(),
 });
 
-// Generate basic CRUD API for that schema.
-const {
+// Export generated TypeScript types for use in app.
+
+// The complete Todo type as defined by `todoSchema` above.
+export type Todo = z.infer<typeof todoSchema>;
+
+// A version of the Todo type where every field except `id` is optional. Used
+// by the generated `updateTodo` function above.
+export type TodoUpdate = Update<Todo>;
+
+// We define basic typesafe CRUD helpers for reading and writing Todos to
+// Replicache using https://github.com/rocicorp/rails. Again, this is not
+// required (you can read and write directly using e.g., Replicache's lower-level
+// put() and get() APIs), but this gets you typesafety and validation.
+export const {
   put: putTodo,
   update: updateTodo,
   delete: deleteTodo,
   list: listTodos,
 } = generate("todo", todoSchema);
-
-// Export relevant TS types for use in app.
-export type Todo = z.infer<typeof todoSchema>;
-export type TodoUpdate = Update<Todo>;
-
-// The generated put, update, delete, etc. functions can be used as mutators
-// directly because they have the right signature. For simple apps, this is
-// often all you need!
-//
-// But the generated functions also nicely compose into higher-level, more
-// specialized mutators. See createTodo for an example below.
-export { createTodo, updateTodo, deleteTodo, listTodos };
-
-// This specialized mutator creates a new todo, assigning the next available
-// sort value. By implementing this in a mutator, we solve the problem of two
-// clients simultaneously choosing the same sort value.
-//
-// The way this works is that Replicache mutations run at least twice: once on
-// the client, optimistically, against the local datastore; and then once again
-// on the server against the backend datastore.
-//
-// If two clients create new todos concurrently, they both might choose the same
-// sort value locally. That's fine because later when the mutator re-runs on the
-// server the two todos will get unique values. Replicache will automatically
-// sync the change back to the clients, reconcile any changes that happened
-// client-side in the meantime, and update the UI to reflect the changes.
-async function createTodo(tx: WriteTransaction, todo: Omit<Todo, "sort">) {
-  const todos = await listTodos(tx);
-  todos.sort((t1, t2) => t1.sort - t2.sort);
-
-  const maxSort = todos.pop()?.sort ?? 0;
-  await putTodo(tx, { ...todo, sort: maxSort + 1 });
-}
