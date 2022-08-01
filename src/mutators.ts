1114f6fbcae0a44418f22683edba49c40cacc1b4
Aaron Boodman <aaron@aaronboodman.com>
2022-07-31 15:54:49 -1000

    Remove rails.
    I received some feedback that it is more confusing than useful to
    have this extra level of abstraction at the point people are just
    trying to use Replicache which makes sense.
    
    Our bigger sample apps will still use Rails, but this sample should
    be as minimal as possible.

--- src/mutators.ts
+++ src/mutators.ts
@@ -1,73 +1,62 @@
 // This file defines our "mutators".
 //
-// Replicache mutators are how you change data in Replicache apps, and are the
-// key enabling idea that makes Replicache work.
+// Mutators are how you change data in Replicache apps.
 //
-// Mutators at core are just named functions with a special signature:
-//
-// type Mutator<Args, Ret> = (tx: WriteTransaction, args: Args) => Promise<Ret>;
-//
-// For example:
-// async function createTodo(tx: WriteTransaction, args: { text: string }) ...
-// is a mutator.
-//
-// Mutators are registered with Replicache at construction-time and callable
-// like `myReplicache.mutate.createTodo({text: "foo"})`.
+// They are registered with Replicache at construction-time and callable like:
+// `myReplicache.mutate.createTodo({text: "foo"})`.
 //
 // Replicache runs each mutation immediately (optimistically) on the client,
-// against the local cache, and then later (usually moments later) *re-runs* it
-// on the server-side against the backend datastore during sync.
+// against the local cache, and then later (usually moments later) sends a
+// description of the mutation (its name and arguments) to the server, so that
+// the server can *re-run* the mutation there against the authoritative
+// datastore.
 //
 // This re-running of mutations is how Replicache handles conflicts: the
-// mutators themselves defensively check the database when they run and do the
-// appropriate thing. The Replicache sync protocol ensures that the server-side
-// result takes precedence over the client-side optimistic result.
+// mutators defensively check the database when they run and do the appropriate
+// thing. The Replicache sync protocol ensures that the server-side result takes
+// precedence over the client-side optimistic result.
 //
-// In JavaScript-based Replicache apps, it's common to reuse mutator functions
-// on both client and server. This sample demonstrates the pattern by using
-// these mutators both with Replicache on the client (see app.tsx) and
-// on the server (see /pages/api/replicache/[op].ts).
+// If the server is written in JavaScript, the mutator functions can be directly
+// reused on the server. This sample demonstrates the pattern by using these
+// mutators both with Replicache on the client (see app.tsx) and on the server
+// (see pages/api/replicache/[op].ts).
 //
 // See https://doc.replicache.dev/how-it-works#sync-details for all the detail
 // on how Replicache syncs and resolves conflicts, but understanding that is not
 // required to get up and running.
 import { WriteTransaction } from "replicache";
-import { Todo, putTodo, updateTodo, deleteTodo, listTodos } from "./todo";
+import { Todo, listTodos, TodoUpdate } from "./todo";
 
 export type M = typeof mutators;
 
-// We define three mutators.
-//
-// updateTodo and deleteTodo are just re-exported CRUD functions generated in
-// todo.ts.
-//
-// However, it's also possible to define specialized mutators that have more
-// interesting conflict-resolution logic. See `createTodo` below for an example
-// of that.
 export const mutators = {
-  updateTodo,
-  deleteTodo,
-  createTodo,
+  updateTodo: async (tx: WriteTransaction, update: TodoUpdate) => {
+    // In a real app you may want to validate the incoming data is in fact a
+    // TodoUpdate. Check out https://www.npmjs.com/package/@rocicorp/rails for
+    // some heper functions to do this.
+    const prev = (await tx.get(update.id)) as Todo;
+    const next = { ...prev, ...update };
+    await tx.put(next.id, next);
+  },
+
+  deleteTodo: async (tx: WriteTransaction, id: string) => {
+    await tx.del(id);
+  },
+
+  // This mutator creates a new todo, assigning the next available sort value.
+  //
+  // If two clients create new todos concurrently, they both might choose the
+  // same sort value locally (optimistically). That's fine because later when
+  // the mutator re-runs on the server the two todos will get unique values.
+  //
+  // Replicache will automatically sync the change back to the clients,
+  // reconcile any changes that happened client-side in the meantime, and update
+  // the UI to reflect the changes.
+  createTodo: async (tx: WriteTransaction, todo: Omit<Todo, "sort">) => {
+    const todos = await listTodos(tx);
+    todos.sort((t1, t2) => t1.sort - t2.sort);
+
+    const maxSort = todos.pop()?.sort ?? 0;
+    await tx.put(todo.id, { ...todo, sort: maxSort + 1 });
+  },
 };
-
-// This specialized mutator creates a new todo, assigning the next available
-// sort value. By implementing this in a mutator, we solve the problem of two
-// clients simultaneously choosing the same sort value.
-//
-// If two clients create new todos concurrently, they both might choose the same
-// sort value locally (optimistically). That's fine because later when the
-// mutator re-runs on the server the two todos will get unique values.
-//
-// Replicache will automatically sync the change back to the clients, reconcile
-// any changes that happened client-side in the meantime, and update the UI to
-// reflect the changes.
-export async function createTodo(
-  tx: WriteTransaction,
-  todo: Omit<Todo, "sort">
-) {
-  const todos = await listTodos(tx);
-  todos.sort((t1, t2) => t1.sort - t2.sort);
-
-  const maxSort = todos.pop()?.sort ?? 0;
-  await putTodo(tx, { ...todo, sort: maxSort + 1 });
-}
--- src/todo.ts
+++ src/todo.ts
@@ -1,35 +1,14 @@
-// This file defines our "Todo" entity.
-// You will generally have one such file for each entity in your application.
+import { ReadTransaction } from "replicache";
 
-import { z } from "zod";
-import { entitySchema, generate, Update } from "@rocicorp/rails";
+export type Todo = {
+  id: string;
+  text: string;
+  completed: boolean;
+  sort: number;
+};
 
-// We define our datatypes using https://zod.dev/. This isn't required to use
-// Replicache, but is recommended. It gets you free parsing/validation of JSON
-// into strong TypeScript types and is also used below to generate helper CRUD
-// functions.
-export const todoSchema = entitySchema.extend({
-  text: z.string(),
-  completed: z.boolean(),
-  sort: z.number(),
-});
+export type TodoUpdate = Partial<Todo> & Pick<Todo, "id">;
 
-// Export generated TypeScript types for use in app.
-
-// The complete Todo type as defined by `todoSchema` above.
-export type Todo = z.infer<typeof todoSchema>;
-
-// A version of the Todo type where every field except `id` is optional. Used
-// by the generated `updateTodo` function above.
-export type TodoUpdate = Update<Todo>;
-
-// We define basic typesafe CRUD helpers for reading and writing Todos to
-// Replicache using https://github.com/rocicorp/rails. Again, this is not
-// required (you can read and write directly using e.g., Replicache's lower-level
-// put() and get() APIs), but this gets you typesafety and validation.
-export const {
-  put: putTodo,
-  update: updateTodo,
-  delete: deleteTodo,
-  list: listTodos,
-} = generate("todo", todoSchema);
+export async function listTodos(tx: ReadTransaction) {
+  return (await tx.scan().values().toArray()) as Todo[];
+}
